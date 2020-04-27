d3d39282f616b352210a67487d9c1bc5937a27eb
Kenneth-Brandon <onegiantsfan25@yahoo.com>
2020-04-27 13:48:13 -0700

    first step

---
+++ friends/src/components/Login.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: '',
+      password: '',
+    },
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [event.target.name]: event.target.value,
+      },
+    });
+  };
+
+  login = (event) => {
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Sign In</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
